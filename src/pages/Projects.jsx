480c440883014f7cc3ea058833e3a72b1bf1a136
Welington Silva <welingtonsilva401@gmail.com>
2024-12-20 06:39:01 -0300

    atualizações na página de projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "../styles/Projects.css";
 
+import { useState } from "react";
+
 import HackathonPorject from "../img/home-ods.png";
 import ImcCalcHome from "../img/home-imc.jpeg";
 import ImcCalcResult from "../img/result-imc.jpeg";
 import PortfolioCPDI from "../img/gif.gif";
 import SaudeProject from "../img/+saude.png";
+import PicPayHome from "../img/picpay-home.jpeg";
+import PicPayWallet from "../img/picpay-wallet.jpeg";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
+  const handleDownload = (name) => {
+    if (name === "picpay") {
+      window.location.href = "/portfolio-oficial/app.apk"; 
+    } else if (name === "imc") {
+      window.location.href = "/portfolio-oficial/calc.apk";
+    }
+  };
+
   return (
     <section id="projects">
       <div className="projects-container">
@@ -52,13 +64,12 @@ const Projects = () => {
             uma ferramenta útil e educativa para promover hábitos saudáveis.
           </p>
 
-          <Link
+          <button
             className="link-btn"
-            target="_blank"
-            to="https://welington1209.github.io/Hackaton-CPDI-ODS/"
+            onClick={() => handleDownload("imc")}
           >
-            Visitar Site
-          </Link>
+            Baixar Apk
+          </button>
         </div>
 
         <div className="project">
@@ -90,10 +101,14 @@ const Projects = () => {
           <img src={SaudeProject} alt="" />
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero
-            dicta soluta praesentium? In atque quis cupiditate consequuntur
-            minus, odio beatae nobis placeat veniam unde voluptatibus soluta
-            impedit hic
+            O projeto +Saúde foi criado como parte da formatura do curso CPDI,
+            com a ideia de ser um site para serviços médicos. Ele permite que os
+            usuários acessem seus exames, acompanhem consultas e encontrem
+            profissionais de confiança de forma fácil e prática. O site tem um
+            visual simples e moderno, pensado para facilitar a navegação e
+            tornar a experiência do usuário mais agradável. Tudo foi feito com
+            cuidado para que seja útil no dia a dia e ajude as pessoas a
+            cuidarem melhor da saúde.
           </p>
 
           <Link
@@ -106,16 +121,28 @@ const Projects = () => {
         </div>
 
         <div className="project">
-          <h4>Nome do projeto</h4>
+          <h4>PicPay Clone</h4>
 
-          <img src="https://placehold.co/100" alt="" />
+          <div>
+            <img src={PicPayHome} alt="" />
+            <img src={PicPayWallet} alt="" />
+          </div>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero
-            dicta soluta praesentium? In atque quis cupiditate consequuntur
-            minus, odio beatae nobis placeat veniam unde voluptatibus soluta
-            impedit hic
+            Desenvolvido como um aplicativo para simular funcionalidades de uma
+            carteira digital. Nele, o usuário começa com um saldo inicial
+            fictício de R$ 100,00 e pode realizar transações simuladas, como
+            pagamentos via Pix. O app permite cadastrar e compartilhar chaves
+            Pix, além de gerar comprovantes fictícios ao final de cada
+            transação, que podem ser baixados em PDF e compartilhados. Com uma
+            interface simples e funcional, o objetivo do projeto é oferecer uma
+            experiência prática e realista de uso, sendo ideal para aprendizado
+            e demonstração de recursos financeiros digitais.
           </p>
+
+          <button className="link-btn" onClick={() => handleDownload("picpay")}>
+            Baixar Apk
+          </button>
         </div>
 
         <div className="project">
