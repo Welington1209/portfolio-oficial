7fef442624ba01020b812bc2a74fdc8c08931074
Welington Silva <welingtonsilva401@gmail.com>
2024-12-23 06:18:11 -0300

    versão simplificada da página de projetos

---
+++ src/data/projectsData.js
@@ -0,0 +1,71 @@
+import HackathonPorject from "../img/home-ods.png";
+import ImcCalcHome from "../img/home-imc.jpeg";
+import ImcCalcResult from "../img/result-imc.jpeg";
+import PortfolioCPDI from "../img/gif.gif";
+import SaudeProject from "../img/+saude.png";
+import PicPayHome from "../img/picpay-home.jpeg";
+import PicPayWallet from "../img/picpay-wallet.jpeg";
+import DevQuiz from "../img/dev-quiz.png";
+import MoviesLib from "../img/movies-lib.png";
+import GitHubFinder from "../img/github-finder.png";
+
+export const projectsData = [
+  {
+    title: "Espaço ODS",
+    img: HackathonPorject,
+    description:
+      '"Site vencedor do hackathon CPDI, focado nos ODS 2 (Fome Zero) e 6 (Água Potável e Saneamento). Apresenta um quiz interativo que desafia os usuários a testarem seus conhecimentos de forma dinâmica e educativa. Desenvolvido como uma SPA, utiliza Context API para gerenciar estados e garantir fluidez na navegação. O design é responsivo e intuitivo, promovendo acessibilidade e engajamento com os temas abordados."',
+    link: "https://welington1209.github.io/Hackaton-CPDI-ODS/",
+  },
+  {
+    title: "Portfolio Ibrahim Memon",
+    img: PortfolioCPDI,
+    description:
+      "Portfólio desenvolvido durante o curso CPDI, baseado em um modelo Figma criado por Ibrahim Memon. Construído com JavaScript puro e integrado a uma biblioteca de animação de scroll, o projeto apresenta um design moderno e chamativo, destacando transições suaves e interatividade. Ideal para exibir projetos de forma profissional e atraente.",
+    link: "https://welington1209.github.io/portfolio-2024/",
+  },
+  {
+    title: "+Saúde",
+    img: SaudeProject,
+    description:
+      "O projeto +Saúde foi criado como parte da formatura do curso CPDI, com a ideia de ser um site para serviços médicos. Ele permite que os usuários acessem seus exames, acompanhem consultas e encontrem profissionais de confiança de forma fácil e prática. O site tem um visual simples e moderno, pensado para facilitar a navegação e tornar a experiência do usuário mais agradável.",
+    link: "https://welington1209.github.io/projeto_CPDI/",
+  },
+  {
+    title: "Movies Lib",
+    img: MoviesLib,
+    description:
+      "Desenvolvido durante o curso Formação Front-end, esta biblioteca de filmes utiliza a API do TMDB para oferecer uma experiência interativa. Funcionalidades principais: Categorias: Filmes mais populares, melhores avaliados e futuros lançamentos. Busca Avançada: Pesquise qualquer filme e acesse detalhes como sinopse, data de lançamento, duração, custo de produção e elenco. Detalhamento Completo: Cada filme possui uma página dedicada com informações detalhadas, organizadas de forma intuitiva.",
+    link: "https://movies-lib-rho.vercel.app/",
+  },
+  {
+    title: "Dev Quiz",
+    img: DevQuiz,
+    description:
+      "Este projeto foi desenvolvido durante o curso Formação Front-end e aprimorado com funcionalidades extras. Trata-se de um quiz temático sobre programação, onde o usuário pode escolher a categoria das perguntas antes de iniciar o jogo, personalizando a experiência; receber dicas relacionadas à pergunta como forma de ajuda; e utilizar a opção de excluir uma alternativa errada.",
+    link: "https://welington1209.github.io/dev_quiz/",
+  },
+  {
+    title: "GitHub Finder",
+    img: GitHubFinder,
+    description:
+      "É uma ferramenta que permite buscar e explorar perfis de usuários no GitHub. Ao inserir um nome de usuário, o sistema exibe informações detalhadas, como o perfil do usuário, seus principais repositórios, seguidores, e estatísticas de atividade. A aplicação oferece uma interface simples e rápida, facilitando a navegação e o acesso ao conteúdo de outros desenvolvedores.",
+    link: "https://welington1209.github.io/dev_quiz/",
+  },
+  {
+    title: "PicPay Clone",
+    img: [PicPayHome, PicPayWallet],
+    description:
+      "Desenvolvido como um aplicativo para simular funcionalidades de uma carteira digital. Nele, o usuário começa com um saldo inicial fictício de R$ 100,00 e pode realizar transações simuladas, como pagamentos via Pix. O app permite cadastrar e compartilhar chaves Pix, além de gerar comprovantes fictícios ao final de cada transação.",
+    link: null,
+    download: "picpay",
+  },
+  {
+    title: "Imc Calculator",
+    img: [ImcCalcHome, ImcCalcResult],
+    description:
+      "Aplicativo desenvolvido com Expo para calcular o Índice de Massa Corporal (IMC) de forma prática. O app exibe o resultado, oferece dicas de saúde personalizadas e permite compartilhar os dados diretamente com amigos ou redes sociais.",
+    link: null,
+    download: "imc",
+  },
+];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,12 +10,18 @@ import PortfolioCPDI from "../img/gif.gif";
 import SaudeProject from "../img/+saude.png";
 import PicPayHome from "../img/picpay-home.jpeg";
 import PicPayWallet from "../img/picpay-wallet.jpeg";
+import DevQuiz from "../img/dev-quiz.png";
+import MoviesLib from "../img/movies-lib.png";
+import GitHubFinder from "../img/github-finder.png";
+
+
+import { projectsData } from "../data/projectsData";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
   const handleDownload = (name) => {
     if (name === "picpay") {
-      window.location.href = "/portfolio-oficial/app.apk"; 
+      window.location.href = "/portfolio-oficial/app.apk";
     } else if (name === "imc") {
       window.location.href = "/portfolio-oficial/calc.apk";
     }
@@ -30,13 +36,13 @@ const Projects = () => {
           <img src={HackathonPorject} alt="" />
 
           <p>
-            Site vencedor do hackathon CPDI, focado nos ODS 2 (Fome Zero) e 6
+            "Site vencedor do hackathon CPDI, focado nos ODS 2 (Fome Zero) e 6
             (Água Potável e Saneamento). Apresenta um quiz interativo que
             desafia os usuários a testarem seus conhecimentos de forma dinâmica
             e educativa. Desenvolvido como uma SPA, utiliza Context API para
             gerenciar estados e garantir fluidez na navegação. O design é
             responsivo e intuitivo, promovendo acessibilidade e engajamento com
-            os temas abordados.
+            os temas abordados."
           </p>
 
           <Link
@@ -46,30 +52,6 @@ const Projects = () => {
           >
             Visitar Site
           </Link>
-        </div>
-
-        <div className="project">
-          <h4>Imc Calculator</h4>
-
-          <div className="imc">
-            <img src={ImcCalcHome} alt="" />
-            <img src={ImcCalcResult} alt="" />
-          </div>
-
-          <p>
-            Aplicativo desenvolvido com Expo para calcular o Índice de Massa
-            Corporal (IMC) de forma prática. O app exibe o resultado, oferece
-            dicas de saúde personalizadas e permite compartilhar os dados
-            diretamente com amigos ou redes sociais. Com um design intuitivo, é
-            uma ferramenta útil e educativa para promover hábitos saudáveis.
-          </p>
-
-          <button
-            className="link-btn"
-            onClick={() => handleDownload("imc")}
-          >
-            Baixar Apk
-          </button>
         </div>
 
         <div className="project">
@@ -121,6 +103,85 @@ const Projects = () => {
         </div>
 
         <div className="project">
+          <h4>Movies Lib</h4>
+
+          <img src={MoviesLib} alt="" />
+
+          <p>
+            Desenvolvido durante o curso Formação Front-end, esta biblioteca de
+            filmes utiliza a API do TMDB para oferecer uma experiência
+            interativa. Funcionalidades principais: Categorias: Filmes mais
+            populares, melhores avaliados e futuros lançamentos. Busca Avançada:
+            Pesquise qualquer filme e acesse detalhes como sinopse, data de
+            lançamento, duração, custo de produção e elenco. Detalhamento
+            Completo: Cada filme possui uma página dedicada com informações
+            detalhadas, organizadas de forma intuitiva. O projeto combina
+            consumo de APIs REST com uma interface dinâmica.
+          </p>
+
+          <Link
+            className="link-btn"
+            target="_blank"
+            to="https://movies-lib-rho.vercel.app/"
+          >
+            Visitar Site
+          </Link>
+        </div>
+
+        <div className="project">
+          <h4>Dev Quiz</h4>
+
+          <img src={DevQuiz} alt="" />
+
+          <p>
+            Este projeto foi desenvolvido durante o curso Formação Front-end e
+            aprimorado com funcionalidades extras. Trata-se de um quiz temático
+            sobre programação, onde o usuário pode: Escolher a categoria das
+            perguntas antes de iniciar o jogo, personalizando a experiência;
+            Receber dicas relacionadas à pergunta como forma de ajuda; Utilizar
+            a opção de excluir uma alternativa errada, aumentando as chances de
+            acertar; Além das funcionalidades aprendidas no curso, foram
+            adicionadas melhorias personalizadas, como a possibilidade de
+            alternar categorias de forma dinâmica, refinamento no design
+            responsivo, e efeitos interativos para tornar a experiência mais
+            envolvente.
+          </p>
+
+          <Link
+            className="link-btn"
+            target="_blank"
+            to="https://welington1209.github.io/dev_quiz/"
+          >
+            Visitar Site
+          </Link>
+        </div>
+
+        <div className="project">
+          <h4>GitHub Finder</h4>
+
+          <img src={GitHubFinder} alt="" />
+
+          <p>
+            É uma ferramenta que permite buscar e explorar perfis de usuários no
+            GitHub. Ao inserir um nome de usuário, o sistema exibe informações
+            detalhadas, como o perfil do usuário, seus principais repositórios,
+            seguidores, e estatísticas de atividade. A aplicação oferece uma
+            interface simples e rápida, facilitando a navegação e o acesso ao
+            conteúdo de outros desenvolvedores. Utiliza a API pública do GitHub
+            para coletar e exibir os dados relevantes de forma dinâmica e
+            eficiente.
+          </p>
+
+          <Link
+            className="link-btn"
+            target="_blank"
+            to="https://welington1209.github.io/dev_quiz/"
+          >
+            Visitar Site
+          </Link>
+        </div>
+
+        <div className="project">
           <h4>PicPay Clone</h4>
 
           <div className="picpay">
@@ -146,42 +207,24 @@ const Projects = () => {
         </div>
 
         <div className="project">
-          <h4>Nome do projeto</h4>
+          <h4>Imc Calculator</h4>
 
-          <img src="https://placehold.co/100" alt="" />
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero
-            dicta soluta praesentium? In atque quis cupiditate consequuntur
-            minus, odio beatae nobis placeat veniam unde voluptatibus soluta
-            impedit hic
-          </p>
-        </div>
-
-        <div className="project">
-          <h4>Nome do projeto</h4>
-
-          <img src="https://placehold.co/100" alt="" />
+          <div className="imc">
+            <img src={ImcCalcHome} alt="" />
+            <img src={ImcCalcResult} alt="" />
+          </div>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero
-            dicta soluta praesentium? In atque quis cupiditate consequuntur
-            minus, odio beatae nobis placeat veniam unde voluptatibus soluta
-            impedit hic
+            Aplicativo desenvolvido com Expo para calcular o Índice de Massa
+            Corporal (IMC) de forma prática. O app exibe o resultado, oferece
+            dicas de saúde personalizadas e permite compartilhar os dados
+            diretamente com amigos ou redes sociais. Com um design intuitivo, é
+            uma ferramenta útil e educativa para promover hábitos saudáveis.
           </p>
-        </div>
 
-        <div className="project">
-          <h4>Nome do projeto</h4>
-
-          <img src="https://placehold.co/100" alt="" />
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero
-            dicta soluta praesentium? In atque quis cupiditate consequuntur
-            minus, odio beatae nobis placeat veniam unde voluptatibus soluta
-            impedit hic
-          </p>
+          <button className="link-btn" onClick={() => handleDownload("imc")}>
+            Baixar Apk
+          </button>
         </div>
       </div>
     </section>
