840ff9d882e3043871df139d769cb1b856398cc0
Welington Silva <welingtonsilva401@gmail.com>
2025-01-06 06:33:22 -0300

    estilização da página de contatos

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,14 @@ import "../styles/Contact.css";
 import imageProfile from "../assets/imageProfile.jpeg";
 import { Link } from "react-router-dom";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Contact = () => {
+  const openEmail = (e) => {
+    e.preventDefault();
+    window.location.href = "mailto:welingtonsilva401@gmail.com";
+  };
   return (
     <section id="contact">
       <Link className="btn" to={"/projects"}>
@@ -26,10 +32,48 @@ const Contact = () => {
 
           <div className="contacts">
             <ul>
-              <li>Instagram</li>
-              <li>WhatsApp</li>
-              <li>Linkedin</li>
-              <li>Email</li>
+              <li className="insta">
+                <Link
+                  target="_blank"
+                  to={
+                    "https://www.instagram.com/w.silva_azulejista/profilecard/?igsh=MWNmb2o5b3p1cGR5MQ%3D%3D"
+                  }
+                >
+                  <FaInstagram size={30} color="#E4405F" />
+                  <p>@w.silva_azulejista</p>
+                </Link>
+              </li>
+
+              <li className="wpp">
+                <Link target="_blank" to="https://wa.me/qr/HFHK7TA5II5GF1">
+                  <FaWhatsapp size={30} color="#25D366" />
+                  <p>(48) 9 9155-1028</p>
+                </Link>
+              </li>
+
+              <li className="linkedin">
+                <Link
+                  target="_blank"
+                  to="https://www.linkedin.com/in/welington-silva-9512a5279/"
+                >
+                  <FaLinkedin size={30} color="#0077B5" />
+                  <p>Welington Silva</p>
+                </Link>
+              </li>
+
+              <li className="email">
+                <Link
+                  to="#"
+                  target="_blank"
+                  onClick={(e) => {
+                    e.preventDefault(); 
+                    window.location.href = "mailto:welingtonsilva401@gmail.com"; 
+                  }}
+                >
+                  <HiOutlineMail className="icon" size={30} />
+                  <p>Welingtonsilva401@gmail.com</p>
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
